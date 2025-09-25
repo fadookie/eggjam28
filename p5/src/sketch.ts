8d85b527fe2acd5c927db281f3190685f07b73d0
Eliot Lash <fadookie@gmail.com>
2025-09-24 17:20:59 -0700

    got interaction-based audio playback working, misc progress

--- p5/src/sketch.ts
+++ p5/src/sketch.ts
@@ -1,21 +1,31 @@
 import type * as p5 from "p5";
 
 let music: p5.SoundFile;
+let hasStarted = false;
 
 function preload() {
   soundFormats('ogg');
-  music = loadSound('tripleClickTheme');
+  music = loadSound('../tripleclickjam25/tripleClickTheme');
 }
 
 function setup() {
- // put setup code here
- createCanvas(400, 400);
- music.play();
+  createCanvas(400, 400);
+  background(90);
+  stroke('red');
+  ellipse(0, 0, 50, 50);
 }
 
-function draw() {
-  // put drawing code here
-}
+// function draw() {
+// }
 
 function mouseClicked() {
+  // First click enables audio context
+  if (!hasStarted) {
+    hasStarted = true;
+    userStartAudio();
+    music.play();
+    console.log('first click');
+    return;
+  }
+  console.log('subsequent click');
 }
