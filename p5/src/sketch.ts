defc5a05fecc942a2810de301e90cfa16b4f5f50
Eliot Lash <fadookie@gmail.com>
2025-09-27 01:53:15 -0700

    add basic music track visualization for rhythm game

--- p5/src/sketch.ts
+++ p5/src/sketch.ts
@@ -10,10 +10,12 @@ const musicEvents = [
   1.090,
   1.298,
   1.532,
+
   97.608,
   97.817,
   98.050,
 ] as const;
+
 let musicEventIdx = 0;
 
 let music: p5.SoundFile;
@@ -28,6 +30,20 @@ function preload() {
 
 function setup() {
   createCanvas(800, 800);
+
+  //@ts-expect-error
+  amplitude = new p5.Amplitude();
+  amplitude.smooth(1);
+
+  //@ts-expect-error
+  fft = new p5.FFT();
+
+  resetSketch();
+}
+
+function resetSketch() {
+  hasStarted = false;
+
   rectMode(CENTER);
   ellipseMode(RADIUS);
   textAlign(CENTER);
@@ -41,12 +57,7 @@ function setup() {
 
   strokeWeight(defaultStrokeWeight);
 
-  //@ts-expect-error
-  amplitude = new p5.Amplitude();
-  amplitude.smooth(1);
-
-  //@ts-expect-error
-  fft = new p5.FFT();
+  music.stop();
 }
 
 function draw() {
@@ -75,7 +86,7 @@ function draw() {
   // const volumeLevel = 0.75;
   const smallCircleSize = volumeLevel * 100;
   const mediumCircleSize = (volumeLevel * 100) + defaultStrokeWeight;
-  const largeCircleSize = (volumeLevel * 100) + defaultStrokeWeight * 2;
+  const largeCircleSize = (volumeLevel * 100) + (defaultStrokeWeight * 2);
 
   noFill();
   stroke(creamColor);
@@ -93,13 +104,42 @@ function draw() {
 function handleMusicTrack() {
   if (musicEventIdx > musicEvents.length) return;
 
-  // draw music track and stuff
   push();
+  // process events that need to fire
   const currentTimeS = music.currentTime();
   const nextEventTimeS = musicEvents[musicEventIdx];
   if (nextEventTimeS && currentTimeS >= nextEventTimeS) {
     console.log('NEXT EVENT FIRE', { currentTimeS, nextEventTimeS });
     ++musicEventIdx;
+  }
+
+  // draw music track
+  noStroke();
+
+  translate(50, 50);
+
+  const circleY = 0;
+
+  // cursor
+  fill('blue');
+  circle(0, 0, 25);
+
+  fill('red');
+  const musicTrackXTimeScale = 500;
+  const currentSeekPosX = currentTimeS * musicTrackXTimeScale;
+  for(let i = 0; i < musicEvents.length; ++i) {
+    const musicEventTimeS = musicEvents[i] || 0;
+
+    if (musicEventTimeS < currentTimeS) continue;
+
+    const x = (musicEventTimeS * musicTrackXTimeScale) - currentSeekPosX;
+    circle(x, circleY, 25);
+
+    // debug label
+    push();
+    fill('black');
+    text(i, x, circleY);
+    pop();
   }
   pop();
 }
@@ -114,4 +154,12 @@ function mouseClicked() {
     return;
   }
   console.log('subsequent click');
+}
+
+function keyPressed() {
+  if (key === 'r') {
+    // reset sketch
+    console.log('reset sketch');
+    resetSketch();
+  }
 }
